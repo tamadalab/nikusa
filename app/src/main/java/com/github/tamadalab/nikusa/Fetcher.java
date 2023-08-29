6d7f6942af96e2534355e4bb69f3d48560fffa2c
ji-ua <jinya.nishi24@gmail.com>
2023-08-29 18:50:04 +0900

    implement the fetch function halfway through

--- app/src/main/java/com/github/tamadalab/nikusa/Fetcher.java
+++ app/src/main/java/com/github/tamadalab/nikusa/Fetcher.java
@@ -1,2 +1,51 @@
-package com.github.tamadalab.nikusa;public class Fetcher {
+package com.github.tamadalab.nikusa;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * Fetcher；Argoを利用しデータを取得する
+ */
+public class Fetcher extends Object{
+    /**
+     * Argoを利用し，データを取得する．
+     * @param nameWithOwner リポジトリのオーナー名とリポジトリ名．(owner/name)
+     */
+    public static void fetch(String nameWithOwner) {
+        String currentDirectory = Paths.get(System.getProperty("user.dir"), "/cache").toString();
+        String containerDirectory = Paths.get("home", "Argo", "cache").toAbsolutePath().toString();
+        String dockerImageName = Paths.get("ghcr.io", "ji-ua", "argo:0.0.2").toString();
+
+        List<String> commands = new ArrayList<>();
+        commands.add("docker");
+        commands.add("run");
+        commands.add("-v");
+        commands.add(currentDirectory + ":" + containerDirectory);
+        commands.add(dockerImageName);
+        commands.add(nameWithOwner);
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(commands);
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+
+            // コンテナの標準出力をリアルタイムで読み取る
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            process.waitFor();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/github/tamadalab/nikusa/Main.java
+++ app/src/main/java/com/github/tamadalab/nikusa/Main.java
@@ -4,5 +4,14 @@ package com.github.tamadalab.nikusa;
  * Main
  */
 public class Main extends Object {
-    public static void main(String[] args) {}
+    /**
+     * データ取得・解析
+     * @param args 引数文字列群
+     */
+    public static void main(String[] args) {
+        for (String arg : args) {
+            Fetcher.fetch(arg);
+
+        }
+    }
 }
