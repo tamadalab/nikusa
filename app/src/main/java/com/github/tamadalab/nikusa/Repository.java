c6bc9213a1814b2ab4467ad2ad3e0f26b22fbf33
ji-ua <jinya.nishi24@gmail.com>
2023-08-29 18:47:35 +0900

    create Repository class

--- app/src/main/java/com/github/tamadalab/nikusa/Repository.java
+++ app/src/main/java/com/github/tamadalab/nikusa/Repository.java
@@ -1,2 +1,71 @@
-package com.github.tamadalab.nikusa;public class Repository {
+package com.github.tamadalab.nikusa;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * Repository：元のリポジトリ
+ */
+public class Repository {
+    /**
+     * 元のリポジトリのオーナー名とリポジトリ名
+     */
+    private String nameWithOwner;
+
+    /**
+     * 元のリポジトリのURL
+     */
+    private URL url;
+
+    /**
+     * 生きている可能性のあるFork一覧
+     */
+    private List<Fork> aliveForks;
+
+    public Repository(String nameWithOwner) {
+        setNameWithOwner(nameWithOwner);
+        setUrl();
+    }
+
+    /**
+     * nameWithOwnerのゲッタ
+     */
+    public String getNameWithOwner() {
+        return this.nameWithOwner;
+    }
+
+    /**
+     * nameWithOwnerのセッタ
+     */
+    public void setNameWithOwner(String nameWithOwner) {
+        this.nameWithOwner = nameWithOwner;
+
+        return;
+    }
+
+    /**
+     * urlのゲッタ
+     */
+    public URL getUrl() {
+        return this.url;
+    }
+
+    /**
+     * urlのセッタ
+     */
+    public void setUrl() {
+        try {
+            this.url = new URL("https://github.com/" + this.nameWithOwner);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * aliveForkのゲッタ
+     */
+    public List<Fork> getAliveForks() {
+        return this.aliveForks;
+    }
 }
