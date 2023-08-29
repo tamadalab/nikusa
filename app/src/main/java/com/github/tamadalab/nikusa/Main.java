e51603ebdd3aa7857d9d6cfc2b4965ee4e12443b
ji-ua <jinya.nishi24@gmail.com>
2023-08-29 16:38:47 +0900

    delete unrelated processes

--- app/src/main/java/com/github/tamadalab/nikusa/Main.java
+++ app/src/main/java/com/github/tamadalab/nikusa/Main.java
@@ -3,12 +3,6 @@ package com.github.tamadalab.nikusa;
 /**
  * Main
  */
-public class Main extends Object
-{
-    public static String project = "nikusa";
-
-    public static void main(String[] args)
-    {
-        System.out.println(project);
-    }
+public class Main extends Object {
+    public static void main(String[] args) {}
 }
