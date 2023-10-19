819e29a9ef8e25caca20f33a7fc49b1d3d186371
ji-ua <jinya.nishi24@gmail.com>
2023-10-19 15:06:40 +0900

    Implemented sorting to be two-tiered.

--- app/src/main/java/com/github/tamadalab/nikusa/Sort.java
+++ app/src/main/java/com/github/tamadalab/nikusa/Sort.java
@@ -3,4 +3,4 @@ package com.github.tamadalab.nikusa;
 /**
  * 指定できるソート方法を列挙
  */
-public enum Sort { commitCountAfterFork, commitedDate }
+public enum Sort { commitCountAfterFork, commitedDate ,topPercentFilter}
